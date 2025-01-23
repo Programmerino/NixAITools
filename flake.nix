362c016216bd0040fb8c42f6717ef209ad4f03df
Davis Davalos-DeLosh <davis.davalos.delosh@gmail.com>
2025-01-22 18:35:56 -0700

    Simplifed

--- flake.nix
+++ flake.nix
@@ -1,36 +1,24 @@
 {
   description = "Nix Template";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://programmerino.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "programmerino.cachix.org-1:v8UWI2QVhEnoU71CDRNS/K1CcW3yzrQxJc604UiijjA="
-    ];
-  };
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = inputs @ {
-    flake-parts,
-    ...
-  }:
+  outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux"];
       perSystem = {
-        config,
-        self',
-        inputs',
-        pkgs,
         system,
         ...
-      }: rec {
+      }: let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+        };
+      in rec {
         packages.default = pkgs.hello;
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [nil alejandra git gitg] ++ packages.default.buildInputs ++ packages.default.nativeBuildInputs ++ packages.default.propagatedBuildInputs;
+          packages = with pkgs; [nil alejandra git] ++ packages.default.buildInputs ++ packages.default.nativeBuildInputs ++ packages.default.propagatedBuildInputs;
         };
         formatter = pkgs.alejandra;
       };
