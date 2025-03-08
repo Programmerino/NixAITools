b8012ef101d89664ff210be8ded9bc3786891fb9
Davis Davalos-DeLosh <davis.davalos.delosh@gmail.com>
2025-03-07 23:39:09 -0700

    Bump nixpkgs, add automatic GC prevention mechanism

--- flake.nix
+++ flake.nix
@@ -12,13 +12,31 @@
         system,
         ...
       }: let
+        name = "NixTemplate";
         pkgs = import inputs.nixpkgs {
           inherit system;
         };
+        saveEnv = pkgs.writeShellApplication {
+          name = "saveEnv";
+          text = ''
+            sudo mkdir -p "/nix/var/nix/gcroots/per-user/$USER/saveEnv"
+            sudo ln -sf "$(nix path-info --derivation .#all)" "/nix/var/nix/gcroots/per-user/$USER/saveEnv/${pkgs.lib.strings.escapeShellArg name}"
+          '';
+        };
+        packages_.default = pkgs.hello;
       in rec {
-        packages.default = pkgs.hello;
+        packages = packages_ // {
+          all = pkgs.symlinkJoin {
+            name = "all";
+            paths = pkgs.lib.attrsets.attrValues packages_;
+          };
+        };
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [nil alejandra git] ++ packages.default.buildInputs ++ packages.default.nativeBuildInputs ++ packages.default.propagatedBuildInputs;
+          packages = with pkgs; [nil alejandra git];
+          inputsFrom = [packages.default];
+          shellHook = ''
+              [[ -n $SAVE_ENV ]] && ( ${saveEnv}/bin/saveEnv >/dev/null 2>&1 & )
+          '';
         };
         formatter = pkgs.alejandra;
       };
